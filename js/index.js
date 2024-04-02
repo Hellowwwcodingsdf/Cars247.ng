e027e17dd86fd2d9ebd5d68df26be0ce744f961d
[Slingshot] <michaelachugwo69@gmail.com~>
2024-04-02 14:48:40 +0100

    added cars page and added forgot password modal

--- js/index.js
+++ js/index.js
@@ -1,14 +1,21 @@
 const navBtn = document.getElementById("navBtn");
 navBtn.style.cursor = "pointer";
-document.getElementById("navBar").style.display = "none";
+let navBar = document.getElementById("navBar");
+navBar.style.display = "none";
 
-navBtn.addEventListener("click", () => {
-  let navBar = document.getElementById("navBar");
-  if ((navBar.style.display === "none")) {
-    navBar.style.display = "block";
-    console.log(navBar.style.display);
-  } else {
-    navBar.style.display = "none";
-    console.log(navBar.style.display);
-  }
-});
+navBtn.addEventListener(
+  "click",
+  () => {
+    let navBar = document.getElementById("navBar");
+    if (navBar.style.display === "none") {
+      navBar.style.display = "block";
+      navBtn.src = "images/xMark.png";
+      navBtn.style.height = "25px";
+    } else {
+      navBar.style.display = "none";
+      navBtn.src = "images/List.png";
+      navBtn.style.height = "32px";
+    }
+  },
+  { passive: true }
+);
