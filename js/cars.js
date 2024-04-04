85a3018ad47ef20c7e3cc9cbe0f260213771394f
[Slingshot] <michaelachugwo69@gmail.com~>
2024-04-04 11:40:33 +0100

    finished cars page

--- js/cars.js
+++ js/cars.js
@@ -1,13 +1,16 @@
 let carInfoModal = document.getElementById("carInfoModal");
 let carModalClose = document.getElementById("carModalClose");
 let carInfoBtn = document.querySelectorAll(".seeCarInfo");
+let carsBody = document.getElementById('carsBody')
 
 carModalClose.addEventListener("click", () => {
   carInfoModal.classList.add("d-none");
+  carsBody.classList.remove("d-none");
 });
 
-const openCarModal = (item) => {
+const openCarModal = () => {
   carInfoModal.classList.remove("d-none");
+    carsBody.classList.add("d-none");
 };
 
 carInfoBtn.forEach((item) => {
