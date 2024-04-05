71299e14fd7e8cfefe5096c98506ddb7caab234c
[Slingshot] <michaelachugwo69@gmail.com~>
2024-04-05 14:22:06 +0100

    fixed

--- js/cars.js
+++ js/cars.js
@@ -3,13 +3,12 @@ let carModalClose = document.getElementById("carModalClose");
 let carInfoBtn = document.querySelectorAll(".seeCarInfo");
 let carsBody = document.getElementById("carsBody");
 let carsNav = document.getElementById("carsNav");
-const carsApi = "https://localhost:7116/api/Cars";
 
 carModalClose.addEventListener("click", () => {
   carInfoModal.classList.add("d-none");
   carsBody.classList.remove("d-none");
   carsNav.classList.remove("d-none");
-  window.scrollTo(0, 0);
+  window.scrollTo(0,0)
 });
 
 const openCarModal = () => {
@@ -21,24 +20,3 @@ const openCarModal = () => {
 carInfoBtn.forEach((item) => {
   item.addEventListener("click", openCarModal);
 });
-
-// window.addEventListener("DOMContentLoaded", async () => {
-//   try {
-//     const response = await fetch(carsApi, {
-//       method: "GET",
-//       mode: "no-cors",
-//       headers: {
-//         Accept: "*/*",
-//       },
-//     });
-//     if (!response.ok) {
-//       throw new Error("Unable to connect to API");
-//     }
-//     const data = await response.json();
-//     console.log(data);
-//   } catch (err) {
-//     alert("Unable fetch file. Check internet connection");
-//     console.error(err);
-//   }
-//   console.log(data);
-// });
